648f990b876059eefa68062d37f32dabd0d29bee
6Nail <55398701+6Nail@users.noreply.github.com>
2019-10-11 21:43:02 +0600

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using System.Net.Mail;
+
+
+
+class Program
+{
+    static void Main()
+    {
+        MailAddress from = new MailAddress("tnik8080@gmail.com", "Nik");
+        MailAddress to = new MailAddress("golumvicerlid35@gmail.com");
+        MailMessage m = new MailMessage(from, to);
+        m.Subject = "Тест";
+        m.Body = "<h2>Письмо-тест работы smtp-клиента</h2>";
+        m.IsBodyHtml = true;
+        SmtpClient smtp = new SmtpClient("smtp.gmail.com", 587);
+        smtp.Credentials = new NetworkCredential("tnik8080@gmail.com", "DD123123");
+        smtp.EnableSsl = true;
+        smtp.Send(m);
+        Console.Read();
+    }
+}
